08b17a7330f523dc4fbe72b64d539fe7dd770abc
Matt Juskelis <matthewjuskelis@gmail.com>
2016-05-12 16:22:51 -0700

    Added example

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "ThreadPool.h"
+#include <iostream>
+
+using namespace std;
+
+struct myArgs
+{
+	string myMessage;
+	int myNumber;
+};
+
+void threadPooledMethod(void* arguments);
+
+int main()
+{
+	int NUMBER_OF_THREADS = 5;
+	int NUMBER_OF_REQUESTS = NUMBER_OF_THREADS * 5;
+	ThreadPool myPool(NUMBER_OF_THREADS);
+	
+	for(int i = 0; i < NUMBER_OF_REQUESTS; i++)
+	{
+		myArgs* args = new myArgs;
+		myPool.enqueue(threadPooledMethod, (void*)args);
+	}
+
+	cin.ignore();
+
+	return 0;
+}
+
+void threadPooledMethod(void* arguments)
+{
+	myArgs* args = (myArgs*)arguments;
+	cout << "Message: " << args->myMessage << ":" << args->myNumber << endl;
+}
--- makefile
+++ makefile
@@ -1,11 +1,15 @@
 CFLAGS = -ansi -Wall -Werror -pedantic -std=c++11
 
-all: ThreadPool
+all: ThreadPool Example
 
 ThreadPool: ThreadPool.h ThreadPool.cpp
 	g++ $(CFLAGS) -c ThreadPool.cpp -o ThreadPool
+
+Example: main.cpp ThreadPool
+	g++ $(CFLAGS) main.cpp ThreadPool -o Example
 
 clean:
 	rm -f *.o
 	rm -f *.out
 	rm -f ThreadPool
+	rm -f Example
